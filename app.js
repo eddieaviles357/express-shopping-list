ec98ae4654db10e41b3cc399cb5fe371b4250638
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-14 21:12:07 -0700

    import morgan, added to middleware on every request

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const app = express();
-const itemsRouter = require('./itemsRouter');
 const ExpressError = require('./expressError')
+const itemsRouter = require('./itemsRouter');
+const morgan = require('morgan');
+
+const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 app.use('/items', itemsRouter);
 
 // 404 page
 app.use((req, res, next) => {
     return new ExpressError("Not found", 404);
-})
+});
 
 module.exports = app;
