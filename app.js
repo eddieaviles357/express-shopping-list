d50bb61b5824ee72d3114973539fdafb5f87f210
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-14 21:00:57 -0700

    import expressError

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const itemsRouter = require('./itemsRouter');
+const ExpressError = require('./expressError')
 
 app.use(express.json());
 app.use('/items', itemsRouter);
+
+// 404 page
+app.use((req, res, next) => {
+    return new ExpressError("Not found", 404);
+})
 
 module.exports = app;
