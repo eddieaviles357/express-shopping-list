eaee7ffafaaceedeb24036b536ec3359e3db2960
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-14 21:27:58 -0700

    added error handler

--- app.js
+++ app.js
@@ -9,9 +9,18 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use('/items', itemsRouter);
 
+app.get('/favicon.ico', (req, res) => res.sendStatus(204));
+
 // 404 page
 app.use((req, res, next) => {
     return new ExpressError("Not found", 404);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    return res.json({
+        error: err.message
+    });
 });
 
 module.exports = app;
