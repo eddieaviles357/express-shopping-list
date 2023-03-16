f13331f5586985e8d38a9517bad19867a02f4ec8
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-16 09:25:56 -0700

    get and post route logic added

--- itemsRouter.js
+++ itemsRouter.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const ExpressError = require('./ExpressError');
 const router = new express.Router();
-const items = require('./fakeDB');
+const Item = require('./Item');
 
-// Get /items
-router.get('/', (req, res) => {
+// GET /items
+router.get('/', async(req, res, next) => {
     try {
+        let items = await Item.getItems();
+
         return res.status(200).json({ items })
     } catch (err) {
+        next(err);
+    }
+});
+
+// POST /items
+router.post('/', async(req, res, next) => {
+    try {
+        // throws Express Error if fields not valid
+        let item = new Item(req.body.name, req.body.price);
+        await Item.setItems(item);
+        return res.status(200).json({added: {item: item.name, price: item.price}})
+    } catch(err) {
         next(err);
     }
 });
