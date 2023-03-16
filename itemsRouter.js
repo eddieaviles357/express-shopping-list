df01b9fcf97c15eaa4b80ac22ebb1a0fa2566124
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-16 10:50:28 -0700

    get item name route logic added

--- itemsRouter.js
+++ itemsRouter.js
@@ -3,7 +3,9 @@ const ExpressError = require('./ExpressError');
 const router = new express.Router();
 const Item = require('./Item');
 
+
 // GET /items
+// get all items
 router.get('/', async(req, res, next) => {
     try {
         let items = await Item.getItems();
@@ -13,14 +15,26 @@ router.get('/', async(req, res, next) => {
         next(err);
     }
 });
+// GET /items/:name
+// get a single item by name
+router.get('/:name', async(req, res, next) => {
+    try {
+        // normalize item name and try to retrieve item in fake db
+        let item = await Item.getItem(req.params['name'].toLowerCase());
+        (item) ? res.status(200).json({item}) : res.status(200).json({ message: 'Not found'}) 
+    } catch (err) {
+        next(err);
+    }
+})
 
 // POST /items
+// add an item to items array
 router.post('/', async(req, res, next) => {
     try {
         // throws Express Error if fields not valid
         let item = new Item(req.body.name, req.body.price);
         await Item.setItems(item);
-        return res.status(200).json({added: {item: item.name, price: item.price}})
+        return res.status(200).json({added: {name: item.name, price: item.price}})
     } catch(err) {
         next(err);
     }
